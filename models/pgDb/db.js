153841e96a06f24373979e97620cd3668c87a7bc
Paweł Rostek <pawel.rostek1@gmail.com>
2015-03-15 21:51:44 +0100

    Last commit in rest-api

--- models/pgDb/db.js
+++ models/pgDb/db.js
@@ -1,7 +1,8 @@
 var pg = require('pg');
 
 var Db = function(callback){
-	var pgConnectionLink = "pg://postgres:postgres@localhost:5432/shop";
+	var pgConnectionLink = "postgres://bljccsnbgihfww:Gqb3nJ6BHSAVgOYVkIilfyMGri@ec2-54-195-243-250.eu-west-1.compute.amazonaws.com:5432/de4pfdstdjieuv";
+	// var pgConnectionLink = "pg://postgres:postgres@localhost:5432/shop";
 	pgClient = new pg.Client(pgConnectionLink);
 	pgClient.connect()
 	return callback(pgClient);
